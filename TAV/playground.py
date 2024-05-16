41a25682baeb8427f8d24ebdcf4f96c74919c87d
jonnir <nir.jonathan@gmail.com>
2024-05-16 18:35:13 +0300

    cleanup

--- TAV/playground.py
+++ TAV/playground.py
@@ -36,54 +36,17 @@ subject_statistics, _, _ = window_sizes.load_or_calc()
 subject_activities, _, _, _ = peri_saccade.load_or_calc_reog()
 
 # %%
+#################################
 # SDT for Saccade Onset
 subject_measures, subject_figures, mean_fig = signal_detection.load_or_calc_saccade_onset()
 
-
 # %%
 #################################
-# Saccade Onset Differences
-
-MAX_DIFF = 20
-
-for idx in range(101, 111):
-    s = Subject.load_or_make(idx, tavh.OUTPUT_DIR)
-    et_onset_idxs = np.where(s.create_boolean_event_channel(s._saccade_onset_idxs, enforce_trials=True))[0]
-    reog_onset_idxs = s.calculate_reog_saccade_onset_indices(filter_name='srp', snr=3.5, enforce_trials=True)
-
-    onset_diffs = np.abs(et_onset_idxs - reog_onset_idxs.reshape((-1, 1)))
-    onset_diffs_argmin = onset_diffs.argmin(axis=0).astype(float)   # index of best match
-    onset_diffs_min = onset_diffs.min(axis=0).astype(float)         # minimum difference between ET and REOG onsets
-    onset_diffs_argmin[onset_diffs_min > MAX_DIFF] = np.nan         # set to nan if difference is too large
-    c = Counter(onset_diffs_argmin)
-    print(f"Subject {idx}:\t{str(c.most_common(5))}")
-
-# %%
+# playground
 
 idx = 101
 s = Subject.load_or_make(idx, tavh.OUTPUT_DIR)
 trial_data = s._trial_data
-
-et_onset_idxs = np.where(s.create_boolean_event_channel(s._saccade_onset_idxs, enforce_trials=True))[0]
-reog_onset_idxs = s.calculate_reog_saccade_onset_indices(filter_name='srp', snr=3.5, enforce_trials=True)
-onset_diffs = np.abs(et_onset_idxs - reog_onset_idxs.reshape((-1, 1)))
-onset_diffs_argmin = onset_diffs.argmin(axis=0).astype(float)  # index of best match
-onset_diffs_min = onset_diffs.min(axis=0).astype(float)  # minimum difference between ET and REOG onsets
-onset_diffs_argmin[onset_diffs_min > MAX_DIFF] = np.nan  # set to nan if difference is too large
-match_idxs = np.stack([et_onset_idxs[~np.isnan(onset_diffs_argmin)],
-                       reog_onset_idxs[onset_diffs_argmin[~np.isnan(onset_diffs_argmin)].astype(int)]]).T
-
-
-table = abs(et_onset_idxs[:, None] - reog_onset_idxs[None, :])
-rowwise = np.stack([table.argmin(0), np.arange(table.shape[1])]).T
-colwise = np.stack([np.arange(table.shape[0]), table.argmin(1)]).T
-rc = (rowwise[:, None] == colwise).all(-1).any(1)
-idxs = rowwise[rc]
-out = np.stack([et_onset_idxs[idxs[:, 0]], reog_onset_idxs[idxs[:, 1]]]).T
-
-# TODO: apply this to `window_sizes.py`:
-out = out[abs(np.diff(out, axis=1).flatten()) <= MAX_DIFF]
-
 
 # %%
 #################################
