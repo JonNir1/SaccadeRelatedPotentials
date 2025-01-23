0c4db68670639ea16ac6c90d8e60f154c282e25d
JonNir <jonathan.nir@mail.huji.ac.il>
2025-01-23 16:25:24 +0200

    read EEGEyeNet - plgrnd

--- EEGEyeNet/playground.py
+++ EEGEyeNet/playground.py
@@ -3,11 +3,12 @@ import os
 import numpy as np
 import pandas as pd
 import pickle as pkl
+import mne
 
 from pymatreader import read_mat
 
 
-PATH = r'C:\Users\nirjo\Desktop\EEGEYENET\dots_data\synchronised_min\EP10\EP10_DOTS1_EEG.mat'
+PATH = r'C:\Users\jonathanni\Desktop\EEGEyeNet\dots_data\synchronised_min\EP12\EP12_DOTS5_EEG.mat'
 
 mat = read_mat(PATH)['sEEG']
 
@@ -17,7 +18,7 @@ sampling_rate = mat['srate']
 xmin, xmax = mat['xmin'], mat['xmax']
 
 timestamps = mat['times']
-data = mat['data']
+data = mat['data']      # channel data: num_channels x num_samples
 
 chanlocs = mat['chanlocs']  # channel locations & labels
 channel_labels = chanlocs['labels']
@@ -27,7 +28,20 @@ chaninfo = mat['chaninfo']
 ref = mat['ref']
 
 event = mat['event']  # event (triggers & ET events) information
-urevent = mat['urevent']
 event_df = pd.DataFrame(event)
 
+urevent = mat['urevent']
+urevent_df = pd.DataFrame(urevent).sort_values(by='latency')
+
+
+reject = mat['reject']
+stats = mat['stats']
 etc = mat['etc']
+
+
+
+
+counts = event_df['type'].value_counts()
+counts.index = counts.index.map(lambda val: val.strip())
+zcounts = counts[~np.isin(counts.index, ['L_saccade', 'L_fixation', 'L_blink', 55, 56, 41])]
+print(zcounts.sum())    # should be 1+27*5 = 136 => block number (12/13/14/15/16) + dot number (1,2,...,27) * 5 blocks
