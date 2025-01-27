87fa6c794d073ceafc6eca4aff6a17b8e3967d5c
JonNir <jonathan.nir@mail.huji.ac.il>
2025-01-27 11:02:18 +0200

    round sampling rate

--- utils/calc_utils.py
+++ utils/calc_utils.py
@@ -1,15 +1,22 @@
 import numpy as np
 
 
-def calculate_sampling_rate(milliseconds: np.ndarray) -> float:
+def calculate_sampling_rate(milliseconds: np.ndarray, decimals: int = None) -> float:
     """
     Calculates the sampling rate of the given timestamps in Hz.
     :param milliseconds: timestamps in milliseconds (floating-point, not integer)
+    :param decimals: number of decimal places to round to
     """
     if len(milliseconds) < 2:
         raise ValueError("timestamps must be of length at least 2")
+    if decimals is not None and not isinstance(decimals, int):
+        raise TypeError("decimals must be an integer")
+    if decimals is not None and decimals < 0:
+        raise ValueError("decimals must be non-negative")
     ms_per_sec = 1000
     sr = ms_per_sec / np.median(np.diff(milliseconds))
     if not np.isfinite(sr):
         raise RuntimeError("Error calculating sampling rate")
-    return float(sr)
+    if decimals is None:
+        return float(sr)
+    return round(sr, decimals)
