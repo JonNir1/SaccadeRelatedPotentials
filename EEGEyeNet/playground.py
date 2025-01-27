6e11f1b42f9413ef98de579264f16d4a9ee90c29
JonNir <jonathan.nir@mail.huji.ac.il>
2025-01-27 15:09:55 +0200

    update TODOs

--- EEGEyeNet/playground.py
+++ EEGEyeNet/playground.py
@@ -1,5 +1,4 @@
 import os
-from abc import ABC, abstractmethod
 
 import numpy as np
 import pandas as pd
@@ -14,5 +13,23 @@ from EEGEyeNet.DataModels.Session import DotsSession
 PATH = r'C:\Users\jonathanni\Desktop\EEGEyeNet\dots_data\synchronised_min\EP12\EP12_DOTS5_EEG.mat'
 # PATH = r'C:\Users\nirjo\Desktop\SRP\data\EEGEyeNet\dots_data\sunchronised_min\EP12\EP12_DOTS1_EEG.mat'
 
+mat = read_mat(PATH)['sEEG']
+
 
 ses = DotsSession.from_mat_file(PATH)
+data = ses.get_data()
+ts = ses.get_timestamps()
+locs = ses.get_channel_locations()
+events = ses.get_events()
+
+
+###############################
+# TODO: Create MNE Raw object #
+
+# convert "events" DF to MNE trigger channel and add it to the Info+Raw objects
+
+info = mne.create_info(ch_names=ses.get_channel_labels().tolist(), sfreq=ses.sampling_rate, ch_types=locs['type'])
+raw = mne.io.RawArray(data, info)
+
+#######################
+# TODO: add methods to extract gaze data
