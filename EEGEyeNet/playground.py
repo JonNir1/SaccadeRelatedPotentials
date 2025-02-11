58dd2fe2c98b3561cbe83b9370dfcf1c2c81d337
JonNir <jonathan.nir@mail.huji.ac.il>
2025-02-11 11:01:16 +0200

    nit

--- EEGEyeNet/playground.py
+++ EEGEyeNet/playground.py
@@ -10,8 +10,8 @@ from pymatreader import read_mat
 from EEGEyeNet.DataModels.DotsSession import DotsSession
 
 
-# PATH = r'C:\Users\jonathanni\Desktop\EEGEyeNet\dots_data\synchronised_min\EP12\EP12_DOTS5_EEG.mat'
-PATH = r'C:\Users\nirjo\Desktop\SRP\data\EEGEyeNet\dots_data\sunchronised_min\EP12\EP12_DOTS1_EEG.mat'
+PATH = r'C:\Users\jonathanni\Desktop\EEGEyeNet\dots_data\synchronised_min\EP12\EP12_DOTS5_EEG.mat'    #lab
+# PATH = r'C:\Users\nirjo\Desktop\SRP\data\EEGEyeNet\dots_data\sunchronised_min\EP12\EP12_DOTS1_EEG.mat'  #home
 
 mat = read_mat(PATH)['sEEG']
 
