b899190472dfa75046b4b36e099ec83e40f431f4
jonnir <nir.jonathan@gmail.com>
2025-03-16 17:16:18 +0200

    attempt showing PSD

---
+++ mne_scripts/mne_trial.py
@@ -0,0 +1,23 @@
+from mne.datasets import sample
+from mne.io import read_raw_fif
+
+import matplotlib
+matplotlib.use('TkAgg')     # or 'Qt5Agg'
+
+
+EEG_REF = "EEG 010"
+
+fname = sample.data_path() / "MEG" / "sample" / "sample_audvis_raw.fif"
+raw = read_raw_fif(fname, preload=False)
+raw.crop(0, 60).pick("eeg")
+raw.load_data()
+raw.filter(1., 40.)
+
+# raw.set_eeg_reference("average")
+# spectrum = raw.compute_psd()
+
+raw.set_eeg_reference(ref_channels=[EEG_REF])
+spectrum = raw.copy().compute_psd(n_fft=512, verbose=False, exclude=[EEG_REF])
+
+spectrum.plot(average=False, picks=['eeg', 'eog'])
+
